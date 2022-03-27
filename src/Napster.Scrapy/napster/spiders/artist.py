98259eacb63bd44526d0fee561337032cd5e6eab
KSExtrez <extrez10@gmail.com>
2022-03-26 19:07:01 -0600

    Add ArtistSpider

---
+++ src/Napster.Scrapy/napster/spiders/artist.py
@@ -0,0 +1,27 @@
+import scrapy
+
+from napster.items import Artist
+
+
+class ArtistSpider(scrapy.Spider):
+    name = 'artist'
+    allowed_domains = ['us.napster.com']
+    start_urls = ['https://us.napster.com/artist/billie-eilish']
+
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'napster.pipelines.NapsterPipeline': 100,
+        },
+    }
+
+    def parse(self, response):
+        artist = Artist()
+        metadata = response.css('div.page-metadata')
+
+        artist['id'] = metadata.attrib['meta_artist_id']
+        artist['name'] = metadata.attrib['meta_artist_name']
+        artist['img'] = response.css('.artist-image>img').attrib['src']
+        artist['genres'] = metadata.attrib['meta_genre_id']
+        artist['albums'] = response.css(
+            'div.album-list>div::attr(album_id)').getall()
+        return artist
