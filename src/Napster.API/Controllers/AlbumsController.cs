060d765c1469ca8dbe7b0565b859eb93f5f18e40
KSExtrez <extrez10@gmail.com>
2022-03-28 02:02:09 -0600

    Add Album controller

---
+++ src/Napster.API/Controllers/AlbumsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Napster.Domain.AggregatesModel.AlbumAggregate;
+using Napster.Domain.AggregatesModel.ArtistAggregate;
+using Napster.Domain.AggregatesModel.GenreAggregate;
+
+namespace Napster.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly IAlbumRepository _albumRepository;
+        private readonly IGenreRepository _genreRepository;
+        private readonly IArtistRepository _artistRepository;
+
+        public AlbumsController(IAlbumRepository albumRepository, IArtistRepository artistRepository, IGenreRepository genreRepository)
+        {
+            _albumRepository = albumRepository;
+            _genreRepository = genreRepository;
+            _artistRepository = artistRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAlbums()
+        {
+            var albums = await _albumRepository.GetAllAlbums();
+            return Ok(albums);
+        }
+
+        [HttpGet("{albumId}")]
+        public async Task<IActionResult> GetAlbumById(string albumId)
+        {
+            var album = await _albumRepository.GetAlbumById(albumId);
+
+            if (album is null)
+            {
+                return NotFound();
+            }
+
+
+            return Ok(album);
+        }
+
+        [HttpGet("{albumId}/genres")]
+        public async Task<IActionResult> GetGenresByAlbumId(string albumId)
+        {
+            var album = await _albumRepository.GetAlbumById(albumId);
+
+            if (album is null)
+            {
+                return NotFound();
+            }
+
+            var genres = await _genreRepository.GetGenreByIds(album.GenreIds);
+
+            return Ok(genres);
+        }
+
+        [HttpGet("{albumId}/artist")]
+        public async Task<IActionResult> GetArtistByAlbumId(string albumId)
+        {
+            var album = await _albumRepository.GetAlbumById(albumId);
+
+            if (album is null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _artistRepository.GetArtistById(album.ArtistId);
+
+            if (artist is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(artist);
+        }
+
+        [HttpGet("{albumId}/tracks")]
+        public async Task<IActionResult> GetTracksByAlbumId(string albumId)
+        {
+            var album = await _albumRepository.GetAlbumById(albumId);
+
+            if (album is null)
+            {
+                return NotFound();
+            }
+
+
+            return Ok(album.Tracks);
+        }
+    }
+}
