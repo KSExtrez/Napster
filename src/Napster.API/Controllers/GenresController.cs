30e920c58e06c25afc9b594cb5d94fbeb594a069
KSExtrez <extrez10@gmail.com>
2022-03-27 22:31:39 -0600

    Add Napster API project

---
+++ src/Napster.API/Controllers/GenresController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Napster.Domain.AggregatesModel.GenreAggregate;
+
+namespace Napster.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly IGenreRepository _genreRepository;
+        public GenresController(IGenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        [HttpGet(Name = "GetGenres")]
+        public async Task<IActionResult> GetAllGenres()
+        {
+            var genres = await _genreRepository.GetAllGenres();
+
+            return Ok(genres);
+        }
+    }
+}
---
+++ src/Napster.API/Program.cs
@@ -0,0 +1,34 @@
+using Napster.Domain.AggregatesModel.GenreAggregate;
+using Napster.Infrastructure.DataAccess.Mongo;
+using Napster.Infrastructure.DataAccess.Mongo.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Napster Db
+builder.Services.Configure<NapsterSettings>(builder.Configuration.GetSection("NapsterDatabase"));
+builder.Services.AddSingleton<NapsterContext>();
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
