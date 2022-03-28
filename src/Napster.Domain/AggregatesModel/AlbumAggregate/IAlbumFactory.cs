51672c1c1cde8488aaa99067a233c5e33716387c
KSExtrez <extrez10@gmail.com>
2022-03-27 22:30:54 -0600

    Remove Factories

--- src/Napster.Domain/AggregatesModel/AlbumAggregate/IAlbumFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Napster.Domain.AggregatesModel.AlbumAggregate
-{
-    public interface IAlbumFactory
-    {
-        Album CreateAlbum(string id, string name, Uri imgUrl, DateOnly releaseDate, string label, string artistId, List<Track> tracks, List<string> genreIds);
-
-        Track CreateTrack(string id, string name, string albumId, Uri preview, TimeOnly duration, string artistId);
-    }
-}
